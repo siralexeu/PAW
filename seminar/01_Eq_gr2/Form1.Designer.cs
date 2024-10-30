7eec42999dc079133a29f32fffb820faad35ce2d
alex <alexandru.cavaler2003@gmail.com>
2024-10-30 13:00:02 +0200

    Add files via upload

---
+++ seminar/01_Eq_gr2/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace _01_Eq_gr2 {
+    partial class FormEq {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() 
+        {
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.tbA = new System.Windows.Forms.TextBox();
+            this.tbB = new System.Windows.Forms.TextBox();
+            this.tbC = new System.Windows.Forms.TextBox();
+            this.lblX2P = new System.Windows.Forms.Label();
+            this.lblPlus = new System.Windows.Forms.Label();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblPlus2 = new System.Windows.Forms.Label();
+            this.lblEqZero = new System.Windows.Forms.Label();
+            this.lblX1 = new System.Windows.Forms.Label();
+            this.tbX1R = new System.Windows.Forms.TextBox();
+            this.lblX1Plus = new System.Windows.Forms.Label();
+            this.tbX1i = new System.Windows.Forms.TextBox();
+            this.lbli1 = new System.Windows.Forms.Label();
+            this.lbli2 = new System.Windows.Forms.Label();
+            this.tbX2i = new System.Windows.Forms.TextBox();
+            this.lblX2Plus = new System.Windows.Forms.Label();
+            this.tbX2R = new System.Windows.Forms.TextBox();
+            this.lblX2 = new System.Windows.Forms.Label();
+            this.tbMes = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.Location = new System.Drawing.Point(655, 435);
+            this.btnSolve.Margin = new System.Windows.Forms.Padding(6);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(150, 44);
+            this.btnSolve.TabIndex = 0;
+            this.btnSolve.Text = "Solve";
+            this.btnSolve.UseVisualStyleBackColor = true;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
+            // 
+            // tbA
+            // 
+            this.tbA.Location = new System.Drawing.Point(78, 75);
+            this.tbA.Margin = new System.Windows.Forms.Padding(6);
+            this.tbA.Name = "tbA";
+            this.tbA.Size = new System.Drawing.Size(110, 31);
+            this.tbA.TabIndex = 1;
+            this.tbA.Text = "2";
+            this.tbA.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.tbA.TextChanged += new System.EventHandler(this.tbA_TextChanged);
+            // 
+            // tbB
+            // 
+            this.tbB.Location = new System.Drawing.Point(300, 75);
+            this.tbB.Margin = new System.Windows.Forms.Padding(6);
+            this.tbB.Name = "tbB";
+            this.tbB.Size = new System.Drawing.Size(110, 31);
+            this.tbB.TabIndex = 2;
+            this.tbB.Text = "3";
+            this.tbB.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tbC
+            // 
+            this.tbC.Location = new System.Drawing.Point(508, 75);
+            this.tbC.Margin = new System.Windows.Forms.Padding(6);
+            this.tbC.Name = "tbC";
+            this.tbC.Size = new System.Drawing.Size(110, 31);
+            this.tbC.TabIndex = 3;
+            this.tbC.Text = "2";
+            this.tbC.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblX2P
+            // 
+            this.lblX2P.AutoSize = true;
+            this.lblX2P.Location = new System.Drawing.Point(204, 88);
+            this.lblX2P.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblX2P.Name = "lblX2P";
+            this.lblX2P.Size = new System.Drawing.Size(48, 25);
+            this.lblX2P.TabIndex = 4;
+            this.lblX2P.Text = "X^2";
+            // 
+            // lblPlus
+            // 
+            this.lblPlus.AutoSize = true;
+            this.lblPlus.Location = new System.Drawing.Point(250, 88);
+            this.lblPlus.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblPlus.Name = "lblPlus";
+            this.lblPlus.Size = new System.Drawing.Size(36, 25);
+            this.lblPlus.TabIndex = 5;
+            this.lblPlus.Text = " + ";
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(426, 88);
+            this.lblX.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(26, 25);
+            this.lblX.TabIndex = 6;
+            this.lblX.Text = "X";
+            // 
+            // lblPlus2
+            // 
+            this.lblPlus2.AutoSize = true;
+            this.lblPlus2.Location = new System.Drawing.Point(460, 88);
+            this.lblPlus2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblPlus2.Name = "lblPlus2";
+            this.lblPlus2.Size = new System.Drawing.Size(36, 25);
+            this.lblPlus2.TabIndex = 7;
+            this.lblPlus2.Text = " + ";
+            // 
+            // lblEqZero
+            // 
+            this.lblEqZero.AutoSize = true;
+            this.lblEqZero.Location = new System.Drawing.Point(650, 88);
+            this.lblEqZero.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblEqZero.Name = "lblEqZero";
+            this.lblEqZero.Size = new System.Drawing.Size(48, 25);
+            this.lblEqZero.TabIndex = 8;
+            this.lblEqZero.Text = " = 0";
+            // 
+            // lblX1
+            // 
+            this.lblX1.AutoSize = true;
+            this.lblX1.Location = new System.Drawing.Point(78, 296);
+            this.lblX1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblX1.Name = "lblX1";
+            this.lblX1.Size = new System.Drawing.Size(50, 25);
+            this.lblX1.TabIndex = 9;
+            this.lblX1.Text = "X1=";
+            // 
+            // tbX1R
+            // 
+            this.tbX1R.Location = new System.Drawing.Point(144, 296);
+            this.tbX1R.Margin = new System.Windows.Forms.Padding(6);
+            this.tbX1R.Name = "tbX1R";
+            this.tbX1R.Size = new System.Drawing.Size(140, 31);
+            this.tbX1R.TabIndex = 10;
+            // 
+            // lblX1Plus
+            // 
+            this.lblX1Plus.AutoSize = true;
+            this.lblX1Plus.Location = new System.Drawing.Point(300, 296);
+            this.lblX1Plus.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblX1Plus.Name = "lblX1Plus";
+            this.lblX1Plus.Size = new System.Drawing.Size(36, 25);
+            this.lblX1Plus.TabIndex = 11;
+            this.lblX1Plus.Text = " + ";
+            // 
+            // tbX1i
+            // 
+            this.tbX1i.Location = new System.Drawing.Point(332, 296);
+            this.tbX1i.Margin = new System.Windows.Forms.Padding(6);
+            this.tbX1i.Name = "tbX1i";
+            this.tbX1i.Size = new System.Drawing.Size(140, 31);
+            this.tbX1i.TabIndex = 12;
+            // 
+            // lbli1
+            // 
+            this.lbli1.AutoSize = true;
+            this.lbli1.Location = new System.Drawing.Point(488, 302);
+            this.lbli1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbli1.Name = "lbli1";
+            this.lbli1.Size = new System.Drawing.Size(17, 25);
+            this.lbli1.TabIndex = 13;
+            this.lbli1.Text = "i";
+            // 
+            // lbli2
+            // 
+            this.lbli2.AutoSize = true;
+            this.lbli2.Location = new System.Drawing.Point(488, 369);
+            this.lbli2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbli2.Name = "lbli2";
+            this.lbli2.Size = new System.Drawing.Size(17, 25);
+            this.lbli2.TabIndex = 18;
+            this.lbli2.Text = "i";
+            // 
+            // tbX2i
+            // 
+            this.tbX2i.Location = new System.Drawing.Point(332, 363);
+            this.tbX2i.Margin = new System.Windows.Forms.Padding(6);
+            this.tbX2i.Name = "tbX2i";
+            this.tbX2i.Size = new System.Drawing.Size(140, 31);
+            this.tbX2i.TabIndex = 17;
+            // 
+            // lblX2Plus
+            // 
+            this.lblX2Plus.AutoSize = true;
+            this.lblX2Plus.Location = new System.Drawing.Point(300, 363);
+            this.lblX2Plus.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblX2Plus.Name = "lblX2Plus";
+            this.lblX2Plus.Size = new System.Drawing.Size(36, 25);
+            this.lblX2Plus.TabIndex = 16;
+            this.lblX2Plus.Text = " + ";
+            // 
+            // tbX2R
+            // 
+            this.tbX2R.Location = new System.Drawing.Point(144, 363);
+            this.tbX2R.Margin = new System.Windows.Forms.Padding(6);
+            this.tbX2R.Name = "tbX2R";
+            this.tbX2R.Size = new System.Drawing.Size(140, 31);
+            this.tbX2R.TabIndex = 15;
+            // 
+            // lblX2
+            // 
+            this.lblX2.AutoSize = true;
+            this.lblX2.Location = new System.Drawing.Point(78, 363);
+            this.lblX2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblX2.Name = "lblX2";
+            this.lblX2.Size = new System.Drawing.Size(50, 25);
+            this.lblX2.TabIndex = 14;
+            this.lblX2.Text = "X2=";
+            // 
+            // tbMes
+            // 
+            this.tbMes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.tbMes.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tbMes.ForeColor = System.Drawing.Color.Black;
+            this.tbMes.Location = new System.Drawing.Point(0, 631);
+            this.tbMes.Margin = new System.Windows.Forms.Padding(3, 4, 3, 3);
+            this.tbMes.Multiline = true;
+            this.tbMes.Name = "tbMes";
+            this.tbMes.Size = new System.Drawing.Size(936, 107);
+            this.tbMes.TabIndex = 22;
+            // 
+            // FormEq
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 738);
+            this.Controls.Add(this.tbMes);
+            this.Controls.Add(this.lbli2);
+            this.Controls.Add(this.tbX2i);
+            this.Controls.Add(this.lblX2Plus);
+            this.Controls.Add(this.tbX2R);
+            this.Controls.Add(this.lblX2);
+            this.Controls.Add(this.lbli1);
+            this.Controls.Add(this.tbX1i);
+            this.Controls.Add(this.lblX1Plus);
+            this.Controls.Add(this.tbX1R);
+            this.Controls.Add(this.lblX1);
+            this.Controls.Add(this.lblEqZero);
+            this.Controls.Add(this.lblPlus2);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.lblPlus);
+            this.Controls.Add(this.lblX2P);
+            this.Controls.Add(this.tbC);
+            this.Controls.Add(this.tbB);
+            this.Controls.Add(this.tbA);
+            this.Controls.Add(this.btnSolve);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "FormEq";
+            this.Text = "Formular rezolvare eq grad 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.TextBox tbA;
+        private System.Windows.Forms.TextBox tbB;
+        private System.Windows.Forms.TextBox tbC;
+        private System.Windows.Forms.Label lblX2P;
+        private System.Windows.Forms.Label lblPlus;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblPlus2;
+        private System.Windows.Forms.Label lblEqZero;
+        private System.Windows.Forms.Label lblX1;
+        private System.Windows.Forms.TextBox tbX1R;
+        private System.Windows.Forms.Label lblX1Plus;
+        private System.Windows.Forms.TextBox tbX1i;
+        private System.Windows.Forms.Label lbli1;
+        private System.Windows.Forms.Label lbli2;
+        private System.Windows.Forms.TextBox tbX2i;
+        private System.Windows.Forms.Label lblX2Plus;
+        private System.Windows.Forms.TextBox tbX2R;
+        private System.Windows.Forms.Label lblX2;
+        private System.Windows.Forms.TextBox tbMes;
+    }
+}
+
---
+++ seminar/01_Eq_gr2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01_Eq_gr2 
+{
+    public partial class FormEq : Form 
+    {
+        public FormEq() 
+        {
+            InitializeComponent();
+            Toate = new Control[] 
+            { 
+                                    lblX1, tbX1R, lblX1Plus, tbX1i, lbli1,
+                                    lblX2, tbX2R, lblX2Plus, tbX2i, lbli2,
+             };
+            foreach(Control crt in Toate) crt.Visible = false;
+        }
+        Control[] Toate;
+
+        private void btnSolve_Click(object sender, EventArgs e) 
+        {
+            double delta = 0;
+            double a = double.Parse(tbA.Text);
+            double b = double.Parse(tbB.Text);
+            double c = double.Parse(tbC.Text);
+            delta = b * b - 4 * a * c;
+            //ecuatie grad 1
+            if (a == 0) {
+                tbMes.Text = "\r\nEcuatie de grad 1";
+                tbX1R.Visible = true; lblX1.Visible = true;
+                tbX1R.Text = (-c / b).ToString("f3");
+            }
+            else {
+                //Ecuatie de grad 2
+                delta = b * b - 4 * a * c;
+                if (delta >= 0){
+                    tbMes.Text = "\r\nEcuatie de grad 2 cu radacini reale";
+                    tbX1R.Visible = true; lblX1.Visible = true;
+                    tbX2R.Visible = true; lblX2.Visible = true;
+                    tbX1R.Text = ((-b + Math.Sqrt(delta)) / (2 * a)).ToString("f3");
+                    tbX2R.Text = ((-b - Math.Sqrt(delta)) / (2 * a)).ToString("f3");
+                }
+                else
+                {
+                    tbMes.Text = "\r\nEcuatie de grad 2, cu radacini complexe";
+                    foreach (Control crt in Toate) crt.Visible = true;
+                    tbX1R.Text = ((-b) / (2 * a)).ToString("f3");
+                    tbX2R.Text = ((-b) / (2 * a)).ToString("f3");
+                    tbX1i.Text = (Math.Sqrt(-delta) / (2 * a)).ToString("f3");
+                    tbX2i.Text = (-Math.Sqrt(-delta) / (2 * a)).ToString("f3");
+                }
+           
+            }
+        }
+
+        private void tbA_TextChanged(object sender, EventArgs e) 
+        {
+            foreach (Control crt in Toate) crt.Visible = true;
+        }
+    }
+}
